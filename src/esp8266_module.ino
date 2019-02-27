8c3b4b0708890f5f9062bc34bc031a1f73dc50af
Jesús Magallón <magallonj23@gmail.com>
2019-02-26 20:17:15 -0600

    Create esp8266_module.ino

---
+++ src/esp8266_module.ino
@@ -0,0 +1,49 @@
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+
+ESP8266WebServer server(80);
+const char* host = "probatium-bt0001";
+String dataJson = "{\"version\":\"1.0\",\"height\":0, \"weight\":0}";
+
+void setup() {
+    Serial.begin(57600);
+    MDNS.begin(host);
+    WiFiManager wifiManager;
+    //wifiManager.resetSettings();
+    wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+    if (!wifiManager.autoConnect("probatium.io")) {
+      delay(3000);
+      ESP.reset();
+      delay(5000);
+    }
+
+    server.on("/data.json", HTTP_GET, []() {
+      server.sendHeader("Connection", "close");
+      server.send(200, "application/json", dataJson);
+    });
+    server.on("/clear-data", HTTP_GET, []() {
+      server.sendHeader("Connection", "close");
+      //wifiManager.resetSettings();
+      ESP.reset();
+      server.send(200, "application/json", "{\"code\":\"OK\"}");
+    });
+    server.begin();
+    MDNS.addService("http", "tcp", 80);
+}
+
+void loop() {
+  getDataFromSerial();
+  server.handleClient();
+  MDNS.update();
+}
+
+void getDataFromSerial() {
+  if (Serial.available()) {
+    Serial.println(dataJSON);
+    dataJson = Serial.readString();
+  }
+}
